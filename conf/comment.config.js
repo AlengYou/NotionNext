827ece7565b9a42046cd02b410e1306b93162e54
Aleng GL <aleng@webglsoft.com>
2025-02-18 04:05:50 +0800

    feat: modify comment config for blog comment

--- conf/comment.config.js
+++ conf/comment.config.js
@@ -53,13 +53,17 @@ module.exports = {
     process.env.NEXT_PUBLIC_COMMENT_CUSDIS_SCRIPT_SRC || '/js/cusdis.es.js', // change this if you're using self-hosted version
 
   // gitalk评论插件 更多参考 https://gitalk.github.io/
-  COMMENT_GITALK_REPO: process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || '', // 你的Github仓库名，例如 'NotionNext'
-  COMMENT_GITALK_OWNER: process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || '', // 你的用户名 e.g tangly1024
-  COMMENT_GITALK_ADMIN: process.env.NEXT_PUBLIC_COMMENT_GITALK_ADMIN || '', // 管理员用户名、一般是自己 e.g 'tangly1024'
+  COMMENT_GITALK_REPO:
+    process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || 'aleng-blog-comment', // 你的Github仓库名，例如 'NotionNext'
+  COMMENT_GITALK_OWNER:
+    process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || 'AlengYou', // 你的用户名 e.g tangly1024
+  COMMENT_GITALK_ADMIN:
+    process.env.NEXT_PUBLIC_COMMENT_GITALK_ADMIN || 'AlengYou', // 管理员用户名、一般是自己 e.g 'tangly1024'
   COMMENT_GITALK_CLIENT_ID:
-    process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_ID || '', // e.g 20位ID ， 在gitalk后台获取
+    process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_ID || 'Ov23lir4ylHHf5ZkqlO5', // e.g 20位ID ， 在gitalk后台获取
   COMMENT_GITALK_CLIENT_SECRET:
-    process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_SECRET || '', // e.g 40位ID， 在gitalk后台获取
+    process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_SECRET ||
+    '7d664df22db7b83d0df26d3e4e16d8fdd53f4268', // e.g 40位ID， 在gitalk后台获取
   COMMENT_GITALK_DISTRACTION_FREE_MODE: false, // 类似facebook的无干扰模式
   COMMENT_GITALK_JS_CDN_URL:
     process.env.NEXT_PUBLIC_COMMENT_GITALK_JS_CDN_URL ||
