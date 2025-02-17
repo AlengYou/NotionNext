ebeeca91bd2119945eec44db7d3cefa6867e62d0
Aleng GL <aleng@webglsoft.com>
2025-02-17 21:29:51 +0800

    feat: remove search icon btn for header

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -28,8 +28,8 @@ const Header = props => {
   const { locale } = useGlobal()
   const router = useRouter()
   const [isOpen, changeShow] = useState(false)
-  const showSearchButton = siteConfig('HEXO_MENU_SEARCH', false, CONFIG)
-  // const showRandomButton = siteConfig('HEXO_MENU_RANDOM', false, CONFIG)
+  // const showSearchButton = siteConfig('HEXO_MENU_SEARCH', false, CONFIG)
+  const showRandomButton = siteConfig('HEXO_MENU_RANDOM', false, CONFIG)
 
   const toggleMenuOpen = () => {
     changeShow(!isOpen)
